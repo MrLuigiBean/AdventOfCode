9baf1c091b2cf7d9269c0da1c3b9b632abbcf167
MrLuigiBean <84760999+MrLuigiBean@users.noreply.github.com>
2025-07-22 01:33:54 +0800

    reading in from file done

--- AoC2024/Day05/main.cpp
+++ AoC2024/Day05/main.cpp
@@ -1,5 +1,49 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+struct Rule
+{
+	int before, after;
+
+	Rule(const std::string& line)
+	{
+		char pipe;
+		std::stringstream(line) >> before >> pipe >> after;
+	}
+};
+
+inline std::ostream& operator<<(std::ostream& stream, const Rule& rule)
+{
+	return stream << '(' << rule.before << '|' << rule.after << ')';
+}
+
+struct Update
+{
+	std::vector<int> pages;
+
+	Update(const std::string& line)
+	{
+		std::stringstream sstream(line);
+		int pageNumber;
+		char comma;
+		do
+		{
+			sstream >> pageNumber;
+			pages.emplace_back(pageNumber);
+		} while (sstream >> comma);
+	}
+};
+
+inline std::ostream& operator<<(std::ostream& stream, const Update& update)
+{
+	stream << '[';
+	for (unsigned i = 0; i < update.pages.size(); ++i)
+		stream << update.pages[i] << ((i < update.pages.size() - 1) ? ',' : '\0');
+	return stream << ']';
+}
 
 int main_01(int argc, char* argv[])
 {
@@ -17,6 +61,35 @@ int main_01(int argc, char* argv[])
 		printf("sorry %s isn't a file\n", argv[1]);
 		return -1;
 	}
+
+	std::vector<Rule> rules;
+	bool isReadingRules = true;
+
+	std::vector<Update> updates;
+	bool isReadingUpdates = false;
+
+	std::string line;
+	while (std::getline(file, line))
+	{
+		if (line.length() == 0)
+		{
+			isReadingRules = false;
+			isReadingUpdates = true;
+			continue;
+		}
+
+		if (isReadingRules)
+			rules.emplace_back(line);
+
+		if (isReadingUpdates)
+			updates.emplace_back(line);
+	}
+
+	printf("All rules:\n");
+	for (const Rule& rule : rules) std::cout << rule << '\n';
+
+	printf("All updates:\n");
+	for (const Update& update : updates) std::cout << update << '\n';
 
 	return 0;
 }
