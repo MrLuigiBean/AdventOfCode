ae0a8b9821b87035bf63242e9c9ee5bb2eeb1678
MrLuigiBean <84760999+MrLuigiBean@users.noreply.github.com>
2025-07-22 00:37:27 +0800

    problems have two parts

--- AoC2024/Day05/main.cpp
+++ AoC2024/Day05/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 
-int main (int argc, char* argv[])
+int main_01(int argc, char* argv[])
 {
 	(void)argc;
 	(void)argv;
-	printf("Hello!\n");
+	printf("Hello 1!\n");
+	return 0;
+}
+
+int main_02(int argc, char* argv[])
+{
+	(void)argc;
+	(void)argv;
+	printf("Hello 2!\n");
+	return 0;
+}
+
+int main(int argc, char* argv[])
+{
+	main_01(argc, argv);
+	// main_02(argc, argv);
 	return 0;
 }
