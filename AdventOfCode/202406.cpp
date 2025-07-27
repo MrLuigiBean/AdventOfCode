4c15df25994cd262c67a089385734c26d667cfc7
MrLuigiBean <84760999+MrLuigiBean@users.noreply.github.com>
2025-07-27 23:06:53 +0800

    basic hello world

---
+++ AdventOfCode/202406.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	constexpr const char* defaultFilename = "small.txt";
+	const char* filename = argc == 2 ? argv[1] : defaultFilename;
+
+	if (argc != 2)
+	{
+		printf("Usage: ./this_program_name some_input_file.txt\n");
+		// return -1;
+		printf("Using default filename %s...\n", defaultFilename);
+	}
+
+	printf("hello world!\n");
+	return 0;
+}
