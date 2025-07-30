6b42d5fd5db0fa5b9f04126aa5ff9a32e9e53a77
MrLuigiBean <84760999+MrLuigiBean@users.noreply.github.com>
2025-07-30 23:11:32 +0800

    hmm... thought I could get it easily... trust!

--- AdventOfCode/202407.cpp
+++ AdventOfCode/202407.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 #include <vector>
 
 #define PRINT(x) std::cout << #x << ": " << (x) << "\n"
@@ -14,7 +15,93 @@ inline std::ostream& operator<<(std::ostream& stream, const std::vector<T>& vec)
 	return stream << ']';
 }
 
-bool ReadDataFromFile(const std::string& filename)
+enum class Operator { ADD, MULTIPLY };
+
+inline std::ostream& operator<<(std::ostream& stream, const Operator& op)
+{
+	return stream << (op == Operator::ADD ? '+' : '*');
+}
+
+struct Equation
+{
+	unsigned testValue = 0; // 2952021131 is too low
+	std::vector<unsigned> numbers;
+	bool isTrueEquation = false;
+
+	Equation(const std::string& str)
+	{
+		std::istringstream sstream{ str };
+		char temp;
+		sstream >> testValue >> temp;
+
+		int number;
+		while (sstream >> number)
+			numbers.emplace_back(number);
+
+		numbers.shrink_to_fit();
+	}
+
+	unsigned ComputeEquation(const std::vector<Operator>& operators) const
+	{
+		unsigned total = numbers.front();
+		for (unsigned i = 0; i < numbers.size() - 1; ++i)
+		{
+			Operator op = operators[i];
+			unsigned number = numbers[i + 1];
+
+			switch (op)
+			{
+			case Operator::ADD:      total += number; break;
+			case Operator::MULTIPLY: total *= number; break;
+			default: break;
+			}
+		}
+		return total;
+	}
+};
+
+inline std::ostream& operator<<(std::ostream& stream, const Equation& eqn)
+{
+	// return stream << "testValue: " << eqn.testValue << ", numbers: " << eqn.numbers;
+	stream << eqn.testValue << ": ";
+	stream << eqn.numbers[0];
+	for (unsigned i = 1; i < eqn.numbers.size(); ++i)
+		stream << ' ' << eqn.numbers[i];
+	return stream;
+}
+
+void FlagTrueEquations(std::vector<Equation>& equations)
+{
+	for (Equation& equation : equations)
+	{
+		std::vector<Operator> operators(equation.numbers.size() - 1);
+
+		unsigned totalCombinations = 1 << operators.size();
+		for (unsigned currentCombination = 0; currentCombination < totalCombinations; ++currentCombination)
+		{
+			for (unsigned i = 0; i < operators.size(); ++i)
+			{
+				unsigned operatorState = (1 << i) & currentCombination;
+
+				// index from the back to match the bits of `currentCombination`
+				unsigned index = operators.size() - 1 - i;
+
+				operators[index] = operatorState ? Operator::MULTIPLY : Operator::ADD;
+			}
+
+			if (equation.ComputeEquation(operators) == equation.testValue)
+			{
+				PRINT(equation.testValue);
+				PRINT(equation.numbers);
+				PRINT(operators);
+				equation.isTrueEquation = true;
+				break;
+			}
+		}
+	}
+}
+
+bool ReadDataFromFile(const std::string& filename, std::vector<Equation>& equations)
 {
 	std::fstream file(filename);
 	if (!file)
@@ -23,6 +110,11 @@ bool ReadDataFromFile(const std::string& filename)
 		return false;
 	}
 
+	std::string line;
+	while (std::getline(file, line))
+		equations.emplace_back(line);
+
+	equations.shrink_to_fit();
 	return true;
 }
 
@@ -38,9 +130,22 @@ int main(int argc, char* argv[])
 		printf("Using default filename %s...\n", defaultFilename);
 	}
 
-	if (!ReadDataFromFile(filename))
+	std::vector<Equation> equations;
+
+	if (!ReadDataFromFile(filename, equations))
 		return -1;
 
-	printf("hello world!\n");
+	FlagTrueEquations(equations);
+
+	unsigned total = 0;
+
+	auto newEnd = std::remove_if(equations.begin(), equations.end(), [](const Equation& eqn)
+		{ return !eqn.isTrueEquation; });
+
+	for (auto it = equations.begin(); it < newEnd; ++it)
+		total += it->testValue;
+
+	PRINT(total);
+
 	return 0;
 }
