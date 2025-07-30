1eff2e7a3bebf7da0dbd198ce82756745dca2952
MrLuigiBean <84760999+MrLuigiBean@users.noreply.github.com>
2025-07-30 18:29:53 +0800

    Day 06 Part 2

--- AdventOfCode/202406.cpp
+++ AdventOfCode/202406.cpp
@@ -14,12 +14,21 @@ inline std::ostream& operator<<(std::ostream& stream, const std::vector<T>& vec)
 	return stream << ']';
 }
 
-enum Symbols
+// C++ scoped enums but not really (https://en.cppreference.com/w/cpp/language/enum.html#Example)
+struct Symbols
 {
-	EMPTY = '.',
-	OBSTRUCTION = '#',
-	START = '^',
-	VISITED = 'X'
+	using Type = int;
+
+	static const Type
+		EMPTY = '.',
+		OBSTRUCTION = '#',
+		START = '^',
+		VISITED = 'X',
+
+		VISITED_N = 0b0001,
+		VISITED_S = 0b0010,
+		VISITED_E = 0b0100,
+		VISITED_W = 0b1000;
 };
 
 enum class Direction { N, S, E, W };
@@ -33,6 +42,18 @@ Direction TurnRight(Direction dir)
 	case Direction::E: return Direction::S;
 	case Direction::W: return Direction::N;
 	default: return Direction::N;
+	}
+}
+
+Symbols::Type VisitedDirection(Direction dir)
+{
+	switch (dir)
+	{
+	case Direction::N: return Symbols::VISITED_N;
+	case Direction::S: return Symbols::VISITED_S;
+	case Direction::E: return Symbols::VISITED_E;
+	case Direction::W: return Symbols::VISITED_W;
+	default: return Symbols::VISITED_N;
 	}
 }
 
@@ -158,6 +179,74 @@ int CountVisitedPositions(Grid& grid)
 	return total;
 }
 
+int CountPossibleObstacles(const Grid& grid)
+{
+	// for each possible obstacle position
+	//   put the obstacle in the board
+	//   let the guard run
+	//     if the guard runs over the same place twice *while going in the same direction*
+	//       count guard as stuck (?)
+
+	unsigned total = 0;
+	for (unsigned i = 0; i < grid.size(); ++i)
+	{
+		for (unsigned j = 0; j < grid[i].size(); ++j)
+		{
+			if (grid[i][j] == Symbols::OBSTRUCTION || grid[i][j] == Symbols::START)
+				continue;
+
+			// copy over grid and add one obstacle to it
+			Grid gridWithObstacle{ grid };
+			gridWithObstacle[i][j] = Symbols::OBSTRUCTION;
+
+			bool loopFound = false;
+
+			Position currentPosition = FindAndClearStartPosition(gridWithObstacle);
+			Direction currentDirection = Direction::N;
+
+			while (currentPosition.IsInGrid() && !loopFound)
+			{
+				Position check = currentPosition;
+				check.Advance(currentDirection);
+
+				bool wasObstructed = false;
+				if (check.IsInGrid())
+				{
+					if (gridWithObstacle[check.row][check.col] == Symbols::OBSTRUCTION)
+					{
+						wasObstructed = true;
+						currentDirection = TurnRight(currentDirection);
+					}
+				}
+
+				if (!wasObstructed)
+				{
+					char& currentSymbol = gridWithObstacle[currentPosition.row][currentPosition.col];
+					Symbols::Type newSymbol = VisitedDirection(currentDirection);
+
+					if (currentSymbol == Symbols::EMPTY)
+					{
+						currentSymbol = newSymbol;
+					}
+					else if (currentSymbol & newSymbol)
+					{
+						std::cout << "WARNING: RE-TREADING SAME DIRECTION AT " << currentPosition << '\n';
+						loopFound = true;
+						++total;
+					}
+					else
+					{
+						currentSymbol |= newSymbol; // bitwise, to overlay directions on top of each other
+					}
+
+					currentPosition.Advance(currentDirection);
+				}
+			}
+		}
+	}
+	return total;
+}
+
 int main(int argc, char* argv[])
 {
 	constexpr const char* defaultFilename = "small.txt";
@@ -175,7 +264,8 @@ int main(int argc, char* argv[])
 	if (!ReadDataFromFile(filename, grid))
 		return -1;
 
-	int total = CountVisitedPositions(grid);
+	// int total = CountVisitedPositions(grid); // part 1
+	int total = CountPossibleObstacles(grid); // part 2
 	PRINT(total);
 
 	return 0;
