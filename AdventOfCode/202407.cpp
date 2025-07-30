e15fba6c6cc725b9a34866ae149bdd2b23ec221e
MrLuigiBean <84760999+MrLuigiBean@users.noreply.github.com>
2025-07-30 18:56:47 +0800

    full boilerplate setup

---
+++ AdventOfCode/202407.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#define PRINT(x) std::cout << #x << ": " << (x) << "\n"
+
+template <typename T>
+inline std::ostream& operator<<(std::ostream& stream, const std::vector<T>& vec)
+{
+	stream << '[';
+	for (unsigned i = 0; i < vec.size(); ++i)
+		stream << vec[i] << ((i < vec.size() - 1) ? "," : "");
+	return stream << ']';
+}
+
+bool ReadDataFromFile(const std::string& filename)
+{
+	std::fstream file(filename);
+	if (!file)
+	{
+		printf("sorry %s isn't a file\n", filename.c_str());
+		return false;
+	}
+
+	return true;
+}
+
+int main(int argc, char* argv[])
+{
+	constexpr const char* defaultFilename = "small.txt";
+	const char* filename = argc == 2 ? argv[1] : defaultFilename;
+
+	if (argc != 2)
+	{
+		printf("Usage: ./this_program_name some_input_file.txt\n");
+		// return -1;
+		printf("Using default filename %s...\n", defaultFilename);
+	}
+
+	if (!ReadDataFromFile(filename))
+		return -1;
+
+	printf("hello world!\n");
+	return 0;
+}
