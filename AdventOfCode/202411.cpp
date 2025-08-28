6a25a2de947eeb3d3920061b059b9527674500d0
MrLuigiBean <84760999+MrLuigiBean@users.noreply.github.com>
2025-08-28 15:16:56 +0800

    wait i have no clue what i'm doing??
    This reverts commit 2bed2cf9b86d1455983bdbaec80ab7b86683b264.

--- AdventOfCode/202411.cpp
+++ AdventOfCode/202411.cpp
@@ -1,4 +1,3 @@
-#include <forward_list>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -22,16 +21,6 @@ inline std::ostream& operator<<(std::ostream& stream, const std::vector<T>& vec)
 
 /// @brief A type used to represent very large integers.
 using BigNumber = unsigned long long;
-
-using Container = std::forward_list<BigNumber>;
-
-inline std::ostream& operator<<(std::ostream& stream, const Container& list)
-{
-	stream << '[';
-	for (Container::const_iterator it = list.begin(); it != list.end(); ++it)
-		stream << *it << (std::next(it) != list.end() ? "," : "");
-	return stream << ']';
-}
 
 /// @brief Determines the number of digits an integer has.
 /// @param number The integer to query.
@@ -58,60 +47,54 @@ BigNumber Pow(BigNumber base, BigNumber power)
 /// @brief Follows three rules on a line of rocks with numbers.
 /// @param numbers The numbers written on the line of rocks.
 /// @param iterations The number of 'blinks' that happen.
-void Iterate(Container& numbers, int iterations)
+void Iterate(std::vector<BigNumber>& numbers, int iterations)
 {
 	struct Insertion
 	{
-		Container::iterator pos;
-		BigNumber number;
-		Insertion(Container::iterator pos_, BigNumber number_) : pos{ pos_ }, number{ number_ } {}
+		BigNumber pos, number;
+		Insertion(BigNumber pos_, BigNumber number_) : pos{ pos_ }, number{ number_ } {}
 	};
 
 	std::vector<Insertion> insertions; // save insertions for after this loop completes
 
 	while (iterations--)
 	{
-		PRINT(iterations);
-
-		// PRINT(numbers);
-
 		insertions.clear();
 
-		for (Container::iterator it = numbers.begin(); it != numbers.end(); ++it)
+		for (unsigned i = 0; i < numbers.size(); ++i)
 		{
-			BigNumber& number = *it;
-
 			// rule 1: zeroes become ones
-			if (number == 0)
+			if (numbers[i] == 0)
 			{
-				number = 1;
+				numbers[i] = 1;
 				continue;
 			}
 
 			// rule 2: split number when digits are even
-			if (int digits = NumberOfDigits(number); digits % 2 == 0)
+			if (int digits = NumberOfDigits(numbers[i]); digits % 2 == 0)
 			{
 				BigNumber secondHalf = 0;
 				for (int secondHalfDigit = 0; secondHalfDigit < (digits / 2); ++secondHalfDigit)
 				{
-					int digit = number % 10;
+					int digit = numbers[i] % 10;
 					secondHalf += digit * Pow(10, secondHalfDigit);
-					number /= 10;
+					numbers[i] /= 10;
 				}
 
 				// at the end, numbers[i] should have the value of firstHalf! :D
 
-				insertions.emplace_back(it, secondHalf);
+				insertions.emplace_back(i + 1, secondHalf);
 
 				continue;
 			}
 
 			// default rule
-			number *= 2024;
+			numbers[i] *= 2024;
 		}
 
-		for (const auto& item : insertions)
-			numbers.insert_after(item.pos, item.number);
+		// do insertions here (in reverse!!!)
+		for (int i = insertions.size() - 1; i >= 0; --i)
+			numbers.insert(numbers.begin() + insertions[i].pos, insertions[i].number);
 	}
 }
 
@@ -119,7 +102,7 @@ void Iterate(Container& numbers, int iterations)
 /// @param filename The name of the file to obtain data from.
 /// @param numbers The numbers read in from the file.
 /// @return true on success, false otherwise.
-bool ReadDataFromFile(const std::string& filename, Container& numbers)
+bool ReadDataFromFile(const std::string& filename, std::vector<BigNumber>& numbers)
 {
 	std::fstream file(filename);
 	if (!file)
@@ -129,11 +112,8 @@ bool ReadDataFromFile(const std::string& filename, Container& numbers)
 	}
 
 	BigNumber number;
-	std::vector<BigNumber> temp;
 	while (file >> number)
-		temp.emplace_back(number);
-
-	numbers = Container(temp.begin(), temp.end());
+		numbers.emplace_back(number);
 
 	return true;
 }
@@ -151,19 +131,13 @@ int main(int argc, char* argv[])
 		printf("Using default filename %s...\n", defaultFilename);
 	}
 
-	Container numbers;
+	std::vector<BigNumber> numbers;
 	if (!ReadDataFromFile(filename, numbers))
 		return -1;
 
 	Iterate(numbers, 75);
 
-	// PRINT(numbers);
-
-	BigNumber size = 0;
-	for (Container::iterator it = numbers.begin(); it != numbers.end(); ++it)
-		++size;
-
-	PRINT(size);
+	PRINT(numbers.size());
 
 	return 0;
 }
